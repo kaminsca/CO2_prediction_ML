87f391359051f8cbb2e293d422b911aff6a1a6f4
kaminsca <clark.a.kaminsky@vanderbilt.edu>
2024-06-21 22:43:33 -0700

    retrieve and format all data from one lat/lon

--- load_from_local.py
+++ load_from_local.py
@@ -1,6 +1,8 @@
 import h5py
 import pandas as pd
 import numpy as np
+import os
+import time
 
 debug = True
 
@@ -38,7 +40,7 @@ def get_relevant_data(file_path):
         co2_num_obs = file['Data/latticeInformation/numObservationPoints'][()]
 
         # consolidate data
-        data = {'date': date,
+        data = {'date': pd.to_datetime(date,format='%m/%Y'),
                 'latitude': lats.flatten(), 
                 'longitude': lons.flatten(),
                 'co2_average': co2_avg.flatten(),
@@ -50,15 +52,66 @@ def get_relevant_data(file_path):
                 }
         df = pd.DataFrame(data)
         return df
-
-if __name__ == '__main__':
-    # setup :)
-    file = '.\data\SWIRL3CO2\GOSATTFTS2009060120090630_03C01SV0305.h5'
     
-    if debug:
-        show_file(file)
-        print('---------------- ------------------')
+def get_data_at_location(latitude, longitude):
+    directory = '.\data\SWIRL3CO2'
+    data_dicts = []
+    array_idx = -1
+    # start = time.time()
 
-    df = get_relevant_data(file)
-    if debug:
-        print(df)
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            with h5py.File(f, 'r') as file:
+                # find lat/lon index on first time around
+                if array_idx == -1:
+                    # get index of location
+                    lats = file['Data/geolocation/latitude'][()].flatten()
+                    lons = file['Data/geolocation/longitude'][()].flatten()
+
+                    lat_idx = np.where(lats == latitude)[0]
+                    lon_idx = np.where(lons == longitude)[0]
+                    array_idx = np.intersect1d(lat_idx, lon_idx)
+                    print("Array index: ", array_idx)
+                    
+                    # Make sure lat_idx and lon_idx are not empty
+                    if not array_idx:
+                        raise ValueError("Latitude or longitude not in dataset")
+
+                # metadata
+                date = str(file['Attribute/metadata/month'][()].item()) + '/' + str(file['Attribute/metadata/year'][()].item())
+                
+                # consolidate data
+                data = {'date': pd.to_datetime(date,format='%m/%Y'),
+                        'latitude': latitude, 
+                        'longitude': longitude,
+                        'co2_average': file['Data/latticeInformation/XCO2BiasCorrectedAverage'][()].flatten()[array_idx][0],
+                        'co2_minimum': file['Data/latticeInformation/XCO2BiasCorrectedMinimum'][()].flatten()[array_idx][0],
+                        'co2_maximum': file['Data/latticeInformation/XCO2BiasCorrectedMaximum'][()].flatten()[array_idx][0],
+                        'co2_median': file['Data/latticeInformation/XCO2BiasCorrectedMedian'][()].flatten()[array_idx][0],
+                        'co2_std_dev': file['Data/latticeInformation/XCO2BiasCorrectedStandardDeviation'][()].flatten()[array_idx][0],
+                        'num_observation_points': file['Data/latticeInformation/numObservationPoints'][()].flatten()[array_idx][0]
+                        }
+                data_dicts.append(data)
+    combined_df = pd.DataFrame(data_dicts)
+    # print("\n--- %s seconds ---" % (time.time() - start))
+    return combined_df
+
+
+# if __name__ == '__main__':
+    # setup :)
+    # file = '.\data\SWIRL3CO2\GOSATTFTS2009060120090630_03C01SV0305.h5'
+    
+    # if debug:
+    #     show_file(file)
+    #     print('---------------- ------------------')
+
+    # df = get_relevant_data(file)
+    # if debug:
+    #     print(df)
+    #     print(df[df.num_observation_points > 0])
+
+    # data = get_data_at_location(83.75, -76.25)
+    # if debug:
+        # print(data)
